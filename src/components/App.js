67243f6937399c7136f36b960d6d0aad5a617957
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2022-12-14 20:30:11 +0300

    preliminary_0

---
+++ src/components/App.js
@@ -0,0 +1,167 @@
+import React, { useEffect, useState } from "react";
+import api from '../utils/api';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
+import EditProfilePopup from './EditProfilePopup';
+import EditAvatarPopup from './EditAvatarPopup';
+import AddPlacePopup from './AddPlacePopup';
+import ImagePopup from './ImagePopup';
+
+function App() {
+
+  // state
+  const [currentUser, setCurrentUser] = useState({});
+  const [cards, setCards] = useState([]);
+  const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  // user & cards setup
+
+  useEffect(() => {
+    api.getUserInfo()
+        .then((profile) => {
+          setCurrentUser(profile);
+        })
+        .catch((err) => {console.log(err);});
+  }, []);
+
+  useEffect(() => {
+    api.getCards()
+        .then((cardsData) => {
+            setCards(cardsData);
+        })
+        .catch((err) => {console.log(err);});
+  }, []);
+
+  // profile handlers
+
+  function handleEditProfileClick() {
+    setEditProfilePopupOpen(true);
+  };
+
+  function handleEditAvatarClick() {
+    setEditAvatarPopupOpen(true);
+  };
+
+  function handleUpdateUser(data) {
+    api.setUserInfo(data.name, data.about)
+    .then((res) => {
+      setCurrentUser(res);
+      closeAllPopups();
+    })
+    .catch((err) => console.log(err))
+  };  
+
+  function handleUpdateAvatar(data) {
+    api.setUserAvatar(data.avatar)
+    .then((res) => {
+      setCurrentUser(res);
+      closeAllPopups();
+    })
+    .catch((err) => console.log(err))
+  };  
+
+  // card handlers
+
+  function handleAddPlaceClick() {
+    setAddPlacePopupOpen(true);
+  };  
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+  };  
+
+  function handleCardLike(card) {
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    api.changeLikeCardStatus(card._id, !isLiked)
+        .then((newCard) => {
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+        })
+        .catch((err) => {console.log(err);});
+  }
+
+  function handleCardDelete(card) {
+      api.deleteCard(card._id)
+          .then(() => {
+          setCards((state) => state.filter((c) => c._id !== card._id));
+          })
+          .catch((err) => {console.log(err);});
+  }
+
+  function handleAddPlaceSubmit(data) {
+    api.saveCard(data.name, data.link)
+    .then((newCard) => {
+      setCards([newCard, ...cards]); 
+      closeAllPopups();
+    })
+    .catch((err) => console.log(err))
+  }
+  
+  // popup close handler
+
+  function closeAllPopups() {
+    setEditAvatarPopupOpen(false);
+    setEditProfilePopupOpen(false);
+    setAddPlacePopupOpen(false);
+    setSelectedCard(null);
+  };
+
+  return (
+    <div className="page">
+      <CurrentUserContext.Provider value={currentUser}>
+
+        <Header />
+
+        <Main 
+          cards={cards}
+          onCardLike={handleCardLike}
+          onCardDelete={handleCardDelete}
+          onEditProfile={handleEditProfileClick} 
+          onAddPlace={handleAddPlaceClick} 
+          onEditAvatar={handleEditAvatarClick} 
+          onCardClick={handleCardClick}
+        />
+
+        <Footer />
+
+        <EditProfilePopup 
+          isOpen={isEditProfilePopupOpen} 
+          onClose={closeAllPopups}
+          onUpdateUser={handleUpdateUser}
+        />
+
+        <EditAvatarPopup 
+          isOpen={isEditAvatarPopupOpen} 
+          onClose={closeAllPopups} 
+          onUpdateAvatar={handleUpdateAvatar}
+        />   
+
+        <AddPlacePopup 
+          isOpen={isAddPlacePopupOpen} 
+          onClose={closeAllPopups} 
+          onAddPlace={handleAddPlaceSubmit}
+        />
+
+        <PopupWithForm 
+          onClose={closeAllPopups} 
+          name="delete-card" 
+          title="Вы уверены?"
+          button="Да"
+        />
+
+        <ImagePopup 
+          card={selectedCard} 
+          onClose={closeAllPopups} 
+        />
+
+      </CurrentUserContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/utils.js
@@ -0,0 +1,20 @@
+export {
+  cardsGallery,
+  gallerySection,
+  buttonEditProfile,
+  formProfile,
+  buttonAddCard,
+  formAddCard,
+  buttonEditAvatar,
+  formEditAvatar,
+  formSettings
+};
+
+const formSettings = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__save-button',
+  inactiveButtonClass: 'form__save-button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__error_visible'
+};
